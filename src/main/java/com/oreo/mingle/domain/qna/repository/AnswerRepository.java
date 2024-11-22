7f4e585b764ab0c159c8f462ae2d544c63c8804f
정재웅 <jweidon0821@gmail.com>
2024-11-23 01:18:05 +0900

    feat: 질문에 대한 답변 조회 기능 구현

--- src/main/java/com/oreo/mingle/domain/qna/repository/AnswerRepository.java
+++ src/main/java/com/oreo/mingle/domain/qna/repository/AnswerRepository.java
@@ -2,6 +2,7 @@ package com.oreo.mingle.domain.qna.repository;
 
 import com.oreo.mingle.domain.qna.entity.Answer;
 import com.oreo.mingle.domain.qna.entity.Question;
+import com.oreo.mingle.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,4 +12,6 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
     List<Answer> findByQuestion(Question question);
 
     int countByQuestion(Question question);
+
+    Optional<Answer> findByUserAndQuestion(User user, Question question);
 }
